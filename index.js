32db80436989f9bc28173e737fde8c3a0b49688c
rvolo <rvolo@users.noreply.github.com>
2021-02-10 21:33:36 -0800

    message formatting

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const openapiDiff = require('openapi-diff');
 const fs = require('fs')
 
 try {
-    const issueNumber = github.context.issue.number;
-
     let baseFile = core.getInput('baseFile', {required: true});
     let headFile = core.getInput('headFile', {required: true});
     let githubToken = core.getInput('github_token', {required: true});
 
-    const result = openapiDiff.diffSpecs({
+    diffSpecs(githubToken, baseFile, headFile);
+
+} catch (error) {
+    console.log(error)
+    core.setFailed(error.message);
+}
+
+function diffSpecs(githubToken, baseFile, headFile) {
+    openapiDiff.diffSpecs({
         sourceSpec: {
             content: fs.readFileSync(baseFile, 'utf8'),
             format: 'openapi3'
@@ -19,24 +25,57 @@ try {
             content: fs.readFileSync(headFile, 'utf8'),
             format: 'openapi3'
         }
-    });
+    }).then((result) => {
+        comment(githubToken, result)
+    })
+}
 
-    let body= JSON.stringify(result);
+function comment(githubToken, openApiResults) {
+    let message = markdownMessage(openApiResults);
 
     const octokit = github.getOctokit(githubToken);
     let {owner, repo} = github.context.repo;
+
     if (core.getInput('repo')) {
         [owner, repo] = core.getInput('repo').split('/');
     }
+
+    const issueNumber = github.context.issue.number;
 
     octokit.issues.createComment({
         owner,
         repo,
         issue_number: issueNumber,
-        body
+        body: message
     });
+}
 
-} catch (error) {
-    console.log(error)
-    core.setFailed(error.message);
+function markdownMessage(openApiResults) {
+    let msg = "";
+
+    if (openApiResults.breakingDifferences.length !== 0) {
+        msg += "##BREAKING API CHANGES\n";
+        msg += "| Action | Path |\n";
+        msg += "|--------|------|\n";
+
+        openApiResults.breakingDifferences.forEach(function (item) {
+            item.sourceSpecEntityDetails.forEach(function (entity){
+                msg += "| " + item.action + " | "+ entity.location+ " |";
+            })
+        })
+    }
+
+    if (openApiResults.nonBreakingDifferences.length !== 0) {
+        msg += "##NON BREAKING API CHANGES\n";
+        msg += "| Action | Path |\n";
+        msg += "|--------|------|\n";
+
+        openApiResults.nonBreakingDifferences.forEach(function (item) {
+            item.sourceSpecEntityDetails.forEach(function (entity){
+                msg += "| " + item.code + " | "+ entity.location+ " |";
+            })
+        })
+    }
+
+    return msg;
 }
