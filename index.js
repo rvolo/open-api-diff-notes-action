82eea6b8b6c9585f0ac08b4c94dfccc8b44a47d8
rvolo <rvolo@users.noreply.github.com>
2021-02-10 20:12:01 -0800

    fixes

--- index.js
+++ index.js
@@ -3,42 +3,38 @@ const github = require('@actions/github');
 const openapiDiff = require('openapi-diff');
 const fs = require('fs')
 
-async function run(): Promise<void> {
-    try {
-        const issueNumber = github.context.issue.number;
+try {
+    const issueNumber = github.context.issue.number;
 
-        let baseFile = core.getInput('baseFile', {required: true});
-        let headFile = core.getInput('headFile', {required: true});
-        let githubToken = core.getInput('github_token', {required: true});
+    let baseFile = core.getInput('baseFile', {required: true});
+    let headFile = core.getInput('headFile', {required: true});
+    let githubToken = core.getInput('github_token', {required: true});
 
-        const result = openapiDiff.diffSpecs({
-            sourceSpec: {
-                content: fs.readFileSync(baseFile, 'utf8'),
-                format: 'openapi3'
-            },
-            destinationSpec: {
-                content: fs.readFileSync(headFile, 'utf8'),
-                format: 'openapi3'
-            }
-        });
-        let resultsJson = JSON.stringify(result);
-
-        const octokit = github.getOctokit(githubToken);
-        let {owner, repo} = github.context.repo;
-        if (core.getInput('repo')) {
-            [owner, repo] = core.getInput('repo').split('/');
+    const result = openapiDiff.diffSpecs({
+        sourceSpec: {
+            content: fs.readFileSync(baseFile, 'utf8'),
+            format: 'openapi3'
+        },
+        destinationSpec: {
+            content: fs.readFileSync(headFile, 'utf8'),
+            format: 'openapi3'
         }
-        await octokit.issues.createComment({
-            owner,
-            repo,
-            issue_number: issueNumber,
-            resultsJson
-        });
+    });
+    let resultsJson = JSON.stringify(result);
 
-    } catch (error) {
-        console.log(error)
-        core.setFailed(error.message);
+    const octokit = github.getOctokit(githubToken);
+    let {owner, repo} = github.context.repo;
+    if (core.getInput('repo')) {
+        [owner, repo] = core.getInput('repo').split('/');
     }
-}
+    octokit.issues.createComment({
+        owner,
+        repo,
+        issue_number: issueNumber,
+        resultsJson
+    });
 
-run();
+} catch (error) {
+    console.log(error)
+    core.setFailed(error.message);
+}
